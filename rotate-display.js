7f42bfacd3b399f02aa79b820741fa6dee2e6bde
Istvan Fedak <istvan.vonfedak@gmail.com>
2020-05-12 22:55:10 -0400

    added api enpoint commands

--- rotate-display.js
+++ rotate-display.js
@@ -1,5 +1,8 @@
 'use string';
 
+const script = '/usr/local/rotate-display/scripts/rotate-display.sh';
+const { execSync } = require('child_process');
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
@@ -10,12 +13,16 @@ const port = 6395;
 const schema = buildSchema(`
   type Query {
     rotateDisplay: String
+    rebootPi: String
   }
 `);
 
 const root = {
   rotateDisplay: () => {
-    return 'It works!';
+    return execSync(`bash ${script}`).toString().slice(0, -1);
+  },
+  rebootPi: () => {
+    return execSync('/sbin/shutdown -r now');
   }
 };
 
